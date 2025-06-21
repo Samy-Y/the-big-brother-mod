f6bfe500321677e65705950cb0e920bc0f501f6d
Samy-Y <youssoufinesamy@gmail.com>
2025-06-21 13:43:33 +0100

    Initial commit

---
+++ src/client/java/com/bigbrother/TheBigBrotherModClient.java
@@ -0,0 +1,10 @@
+package com.bigbrother;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheBigBrotherModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
